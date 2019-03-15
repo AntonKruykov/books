f344db1958dd78ac9e8445bdf9f04609c30a46b4
tony <92kaa@mail.ru>
2019-03-15 21:37:33 +0700

    json api

--- books/api.py
+++ books/api.py
@@ -1,34 +1,54 @@
 from connexion import NoContent
+from flask import jsonify
+from marshmallow import ValidationError
 from sqlalchemy import and_, or_
 
 from books.database import init_db
-from books.models import Book
+from books.models import Book, BookSchema
 
 db_session = init_db()
 
 
-def get_books(author=None, title=None, date_start=None, date_end=None):
+def j(*args, **kwargs):
+    """Wrapper around jsonify that sets the Content-Type of the response to
+    application/vnd.api+json.
+    """
+    response = jsonify(*args, **kwargs)
+    response.mimetype = 'application/vnd.api+json'
+    return response
+
+
+def get_books(filterauthor=None, filtertitle=None, filterdate_start=None,
+              filterdate_end=None):
 
     conditions = []
-    if author:
-        conditions.append(Book.author.like('%{}%'.format(author)))
-    if title:
-        conditions.append(Book.title.like('%{}%'.format(title)))
+    if filterauthor:
+        conditions.append(Book.author.like('%{}%'.format(filterauthor)))
+    if filtertitle:
+        conditions.append(Book.title.like('%{}%'.format(filtertitle)))
 
     date_conditions = []
-    if date_start:
-        date_conditions.append(Book.create_dt >= date_start)
-    if date_end:
-        date_conditions.append(Book.create_dt <= date_end)
+    if filterdate_start:
+        date_conditions.append(Book.create_dt >= filterdate_start)
+    if filterdate_end:
+        date_conditions.append(Book.create_dt <= filterdate_end)
 
     q = db_session.query(Book)
     q = q.filter(or_(or_(*conditions), and_(*date_conditions)))
 
-    return [b.dump() for b in q]
+    data = BookSchema(many=True).dump(q).data
+    return j(data)
 
 
 def add_books(books):
-    for book in books:
+
+    schema = BookSchema(many=True)
+    try:
+        data = schema.load(books).data
+    except ValidationError as err:
+        return j(err.messages), 422
+
+    for book in data:
         db_session.add(Book(**book))
     db_session.commit()
     return NoContent, 201
@@ -36,9 +56,17 @@ def add_books(books):
 
 def edit_book(id, book):
     b = db_session.query(Book).filter(Book.id == id).one_or_none()
+
     if b is None:
         return NoContent, 404
-    db_session.query(Book).filter(Book.id == id).update(book)
+
+    schema = BookSchema()
+    try:
+        data = schema.load(book).data
+    except ValidationError as err:
+        return j(err.messages), 422
+
+    db_session.query(Book).filter(Book.id == id).update(data)
     db_session.commit()
     return NoContent, 200
 
--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from marshmallow_jsonapi.flask import Schema
+from marshmallow_jsonapi import fields
 from sqlalchemy import Column, String, DateTime, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -19,3 +21,15 @@ class Book(Base):
     def dump(self):
         return dict([(k, v) for k, v in vars(self).items()
                      if not k.startswith('_')])
+
+
+class BookSchema(Schema):
+    id = fields.Integer(dump_only=True, as_string=True)
+    title = fields.Str()
+    author = fields.Str()
+    publish_year = fields.Integer()
+    pages_count = fields.Integer()
+    create_dt = fields.DateTime()
+
+    class Meta:
+        type_ = 'books'
